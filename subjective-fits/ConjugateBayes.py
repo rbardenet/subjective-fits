cb28ccc243c8d08700f08c02349a80748d96b1b2
rbardenet <remi.bardenet@gmail.com>
2018-02-14 15:24:52 +0100

    finished ConjugateBayes

--- subjective-fits/ConjugateBayes.py
+++ subjective-fits/ConjugateBayes.py
@@ -1,23 +1,23 @@
 import numpy as np
 import scipy.stats as sps
 
-class ConjugateBayes(data, sigma2Lhd, muPrior=0, sigma2Prior=1):
+class ConjugateBayes:
     """
     Implements Gaussian-Gaussian conjugate model
     """
 
-    def __init__(self):
+    def __init__(self, data, sigma2Lhd, muPrior=0, sigma2Prior=1):
         self.data = data
-        self.sigma2Lhd = sigmaLhd
+        self.sigma2Lhd = sigma2Lhd
         self.muPrior = muPrior
-        self.sigma2Prior =sigmaPrior
+        self.sigma2Prior = sigma2Prior
+        self.N = len(self.data)
 
     def estimate(self):
         """
         returns the posterior mean
         """
-        n = len(self.data)
-        self.muPosterior = 1/(1/self.sigma2Prior+n/self.sigma2Lhd) * (self.muPrior/self.sigma2Prior+
+        self.muPosterior = 1/(1/self.sigma2Prior+self.N/self.sigma2Lhd) * (self.muPrior/self.sigma2Prior
                     +np.sum(self.data)/self.sigma2Lhd)
         return self.muPosterior
 
@@ -26,6 +26,6 @@ class ConjugateBayes(data, sigma2Lhd, muPrior=0, sigma2Prior=1):
         return a centered posterior credible interval with mass >= 1-alpha
         """
         self.estimate()
-        self.sigma2Posterior = 1/(1/self.sigma2Prior+n/self.sigma2Lhd)
-        lo, hi = sps.norm(loc=self.muPosterior, scale=np.sqrt(sigma2Posterior)).interval(alpha)
+        self.sigma2Posterior = 1/(1/self.sigma2Prior+self.N/self.sigma2Lhd)
+        lo, hi = sps.norm(loc=self.muPosterior, scale=np.sqrt(self.sigma2Posterior)).interval(alpha)
         return lo, hi
